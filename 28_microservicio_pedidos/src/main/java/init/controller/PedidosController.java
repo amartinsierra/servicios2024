834034a603841b87e113a94e257318ebdc37a50f
Profesortarde <Profesortarde@A13E100>
2024-02-20 16:45:11 +0100

    proyecto 29

--- 28_microservicio_pedidos/src/main/java/init/controller/PedidosController.java
+++ 28_microservicio_pedidos/src/main/java/init/controller/PedidosController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import init.model.Pedido;
 import init.service.interfaces.PedidosService;
-
+@CrossOrigin("*")
 @RestController
 public class PedidosController {
 	@Autowired
